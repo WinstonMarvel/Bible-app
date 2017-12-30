f9b1cd4fc4793c94ae9ae1e8523e53c21dd0885c
Marvel Jude <Winston.MarvelJude@thomsonreuters.com>
2017-12-30 22:47:28 +0530

    Javascript additions: 	- Changed properties of MyBible() to make them private 	- Added setPage function to update view

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,30 +11,53 @@
 
 function MyBible(version){
 	console.log("Creating Bible");
-	var mybible = this;
-	this.version = version;
-	this.currentBook = "Genesis"; //default
-	this.currentChapter = 1; 
-	this.content = "test";
+	var _this = this;
+	var mybibleContent;
+	var version = version;
+	var currentBook = "Genesis"; //default
+	var currentChapter = 1; 
 
-	this.next_chapter = function(){
-		this.currentChapter += 1;
+	this.nextChapter = function(){
+		currentChapter += 1;
 	}
-	this.previous_chapter = function(){
-		this.currentChapter -= 1;
+
+	this.previousChapter = function(){
+		currentChapter -= 1;
 	}
+
 	this.getCurrentChapter = function(){
-		return this.currentChapter;
+		return currentChapter;
 	}
-	this.LoadBible = function(){
-		$.ajax({url:"/Bible/bible-assets/MSG.json",	success: function(responseTxt){
-		 		mybible.content = responseTxt; //set content to JSON data
-		 		console.log(mybible.content);
+
+	this.setCurrentChapter = function (chaptername){
+		currentChapter = chaptername;
+	}
+
+	this.getBible = function(){
+		$.ajax({
+			url:"/Bible/bible-assets/MSG.json",	
+			success: function(responseTxt){
+		 		mybibleContent = responseTxt; //set content to JSON data
+		 		setPage(responseTxt);
 		}});
+	}
+
+	this.setCurrentBook = function (bookname){
+		currentBook = bookname;
 	}
 }
 
-// var bible = new MyBible("MSG");
-// bible.next_chapter();
+
+function setPage(passageObject){
+	//Convert JSON Object to string and print to screen
+		// 1.	Get Bookname and insert into h2 
+		// 2.	Get Chapter number and append to h2
+		// 3. 	Get Verses and print with verse number in a span next to it.
+}
+
+var bible = new MyBible("MSG");
+bible.getBible();
+
+
 
 
